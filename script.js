33c8e22c0bb9c7c85c88a583712c8d77bd7ea1f5
juancamejoradio <juancamejoradio@gmail.com>
2025-10-27 14:13:15 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const audio = document.getElementById('audioPlayer');
-const playPauseBtn = document.getElementById('playPause');
-
-playPauseBtn.addEventListener('click', () => {
-  const isPaused = audio.paused;
-  isPaused ? audio.play() : audio.pause();
-  playPauseBtn.textContent = isPaused ? '⏸️' : '▶️';
-});
-
